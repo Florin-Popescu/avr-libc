113d1a41226eef4ecfe33c8c8fe5448cb4f0145a
Florin Popescu <florinsgpopescu@gmail.com>
2021-10-05 18:21:27 +0300

    remove permission update

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -3,19 +3,14 @@
 XMLDIR=$HOME/Downloads/atdf
 export XMLDIR
 
-chmod +x ./bootstrap
-chmod +x ./devtools/gen-avr-lib-tree.sh
 ./bootstrap
 
 cd devtools
-chmod +x ./generate_iosym.sh
 ./generate_iosym.sh
-cd ..
 
-mkdir build
-cd build
+mkdir ../build
+cd ../build
 
-chmod +x ../configure
 ../configure \
 --host=avr \
 
